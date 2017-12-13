2726be0ca046217dc1e47f8c0d2c8cc2be7aeb62
lumina-lily <1310332435@qq.com>
2017-12-13 21:54:42 +0800

    a
    aa

--- assets/plugins/pic_tab.js
+++ assets/plugins/pic_tab.js
@@ -2,28 +2,50 @@ var $ = $.noConflict();
 //ban_qh
 $.fn.banqh = function(can){
 	can = $.extend({
-					box:null,//总框架
-					pic:null,//大图框架
-					pnum:null,//小图框架
-					prev_btn:null,//小图左箭头
-					next_btn:null,//小图右箭头
-					prev:null,//大图左箭头
-					next:null,//大图右箭头
-					pop_prev:null,//弹出框左箭头
-					pop_next:null,//弹出框右箭头
-					autoplay:false,//是否自动播放
-					interTime:5000,//图片自动切换间隔
-					delayTime:800,//切换一张图片时间
-					pop_delayTime:800,//弹出框切换一张图片时间
-					order:0,//当前显示的图片（从0开始）
-					picdire:true,//大图滚动方向（true水平方向滚动）
-					mindire:true,//小图滚动方向（true水平方向滚动）
-					min_picnum:null,//小图显示数量
-					pop_up:false,//大图是否有弹出框
-					pop_div:null,//弹出框框架
-					pop_pic:null,//弹出框图片框架
-					pop_xx:null,//关闭弹出框按钮
-					mhc:null//朦灰层
+					// box:null,//总框架
+					// pic:null,//大图框架
+					// pnum:null,//小图框架
+					// prev_btn:null,//小图左箭头
+					// next_btn:null,//小图右箭头
+					// prev:null,//大图左箭头
+					// next:null,//大图右箭头
+					// pop_prev:null,//弹出框左箭头
+					// pop_next:null,//弹出框右箭头
+					// autoplay:false,//是否自动播放
+					// interTime:5000,//图片自动切换间隔
+					// delayTime:800,//切换一张图片时间
+					// pop_delayTime:800,//弹出框切换一张图片时间
+					// order:0,//当前显示的图片（从0开始）
+					// picdire:true,//大图滚动方向（true水平方向滚动）
+					// mindire:true,//小图滚动方向（true水平方向滚动）
+					// min_picnum:null,//小图显示数量
+					// pop_up:false,//大图是否有弹出框
+					// pop_div:null,//弹出框框架
+					// pop_pic:null,//弹出框图片框架
+					// pop_xx:null,//关闭弹出框按钮
+					// mhc:null//朦灰层
+					// box:"#demo1",//总框架
+			    pic:"#ban_pic1",//大图框架
+			    pnum:"#ban_num1",//小图框架
+			    prev_btn:"#prev_btn1",//小图左箭头
+			    next_btn:"#next_btn1",//小图右箭头
+			    pop_prev:"#prev2",//弹出框左箭头
+			    pop_next:"#next2",//弹出框右箭头
+			    prev:"#prev1",//大图左箭头
+			    next:"#next1",//大图右箭头
+			    pop_div:"#demo2",//弹出框框架
+			    pop_pic:"#ban_pic2",//弹出框图片框架
+			    pop_xx:".pop_up_xx",//关闭弹出框按钮
+			    mhc:".mhc",//朦灰层
+			    autoplay:true,//是否自动播放
+			    interTime:5000,//图片自动切换间隔
+			    delayTime:400,//切换一张图片时间
+			    pop_delayTime:400,//弹出框切换一张图片时间
+			    order:0,//当前显示的图片（从0开始）
+			    picdire:true,//大图滚动方向（true为水平方向滚动）
+			    mindire:true,//小图滚动方向（true为水平方向滚动）
+			    min_picnum:4,//小图显示数量
+			    pop_up:true//大图是否有弹出框
 				}, can || {});
 	var picnum = $(can.pic).find('ul li').length;
 	var picw = $(can.pic).find('ul li').outerWidth(true);
