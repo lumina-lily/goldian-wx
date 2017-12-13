e3ace4d25a134e0fdcbe530146d6b97982d596a5
chenxj92 <842674122@qq.com>
2017-12-14 01:24:52 +0800

    add some function of human-exchange page

--- assets/plugins/pic_tab.js
+++ assets/plugins/pic_tab.js
@@ -1,103 +1,43 @@
-var $ = $.noConflict();
+//var $ = $.noConflict();
 //ban_qh
 $.fn.banqh = function(can){
 	can = $.extend({
-					// box:null,//总框架
-					// pic:null,//大图框架
-					// pnum:null,//小图框架
-					// prev_btn:null,//小图左箭头
-					// next_btn:null,//小图右箭头
-					// prev:null,//大图左箭头
-					// next:null,//大图右箭头
-					// pop_prev:null,//弹出框左箭头
-					// pop_next:null,//弹出框右箭头
-					// autoplay:false,//是否自动播放
-					// interTime:5000,//图片自动切换间隔
-					// delayTime:800,//切换一张图片时间
-					// pop_delayTime:800,//弹出框切换一张图片时间
-					// order:0,//当前显示的图片（从0开始）
-					// picdire:true,//大图滚动方向（true水平方向滚动）
-					// mindire:true,//小图滚动方向（true水平方向滚动）
-					// min_picnum:null,//小图显示数量
-					// pop_up:false,//大图是否有弹出框
-					// pop_div:null,//弹出框框架
-					// pop_pic:null,//弹出框图片框架
-					// pop_xx:null,//关闭弹出框按钮
-					// mhc:null//朦灰层
-					// box:"#demo1",//总框架
-			    pic:"#ban_pic1",//大图框架
-			    pnum:"#ban_num1",//小图框架
-			    prev_btn:"#prev_btn1",//小图左箭头
-			    next_btn:"#next_btn1",//小图右箭头
-			    pop_prev:"#prev2",//弹出框左箭头
-			    pop_next:"#next2",//弹出框右箭头
-			    prev:"#prev1",//大图左箭头
-			    next:"#next1",//大图右箭头
-			    pop_div:"#demo2",//弹出框框架
-			    pop_pic:"#ban_pic2",//弹出框图片框架
-			    pop_xx:".pop_up_xx",//关闭弹出框按钮
-			    mhc:".mhc",//朦灰层
-			    autoplay:true,//是否自动播放
-			    interTime:5000,//图片自动切换间隔
-			    delayTime:400,//切换一张图片时间
-			    pop_delayTime:400,//弹出框切换一张图片时间
-			    order:0,//当前显示的图片（从0开始）
-			    picdire:true,//大图滚动方向（true为水平方向滚动）
-			    mindire:true,//小图滚动方向（true为水平方向滚动）
-			    min_picnum:4,//小图显示数量
-			    pop_up:true//大图是否有弹出框
-				}, can || {});
+		box:null,//总框架
+		pic:null,//大图框架
+		pnum:null,//小图框架
+		prev_btn:null,//小图左箭头
+		next_btn:null,//小图右箭头
+		prev:null,//大图左箭头
+		next:null,//大图右箭头
+		autoplay:false,//是否自动播放
+		interTime:5000,//图片自动切换间隔
+		delayTime:800,//切换一张图片时间
+		order:0,//当前显示的图片（从0开始）
+		min_picnum:null//小图显示数量
+	}, can || {});
+	
 	var picnum = $(can.pic).find('ul li').length;
-	var picw = $(can.pic).find('ul li').outerWidth(true);
-
-	// var picw = $(can.pic).find('ul li').width();
-	var pich = $(can.pic).find('ul li').height();
-	var poppicw = $(can.pop_pic).find('ul li').outerWidth(true);
+	var picw = $(can.pic).outerWidth(true);
 	var picminnum = $(can.pnum).find('ul li').length;
-	var picpopnum = $(can.pop_pic).find('ul li').length;
-	var picminw = picw/4+2;
-	var picminh = $(can.pnum).find('ul li').outerHeight(true);
+	var picminw = (picw)/4;
 	var pictime;
 	var tpqhnum=0;
 	var xtqhnum=0;
-	var popnum=0;
-
-	$("#ban_num1 li").width(picw/4-6);
-    $("#ban_num1").height($('#ban_num1 li').height()+12);
-    $('#demo1').height(pich+$("#ban_num1").height());
+	
 	$(can.pic).find('ul').width(picnum*picw);
-	$(can.pnum).find('ul').width(picminnum*picminw + 2);
-	$(can.pop_pic).find('ul').width(picpopnum*poppicw);
-	
 	$(can.pic).find('ul li').width(picw);
-	$("#ban_pic1").height(pich);
+	$(can.pnum).find('ul').width(picminnum*picminw);
+	$(can.pnum).find('ul li').width(picminw-7);
 	
-
-	console.log(picminnum+'*'+picw+'----'+picminw+'====='+picminnum*picminw + 2);
-//点击小图切换大图
-	    $(can.pnum).find('li').click(function () {
+	//点击小图切换大图
+    $(can.pnum).find('li').click(function () {
         tpqhnum = xtqhnum = $(can.pnum).find('li').index(this);
         show(tpqhnum);
 		minshow(xtqhnum);
     }).eq(can.order).trigger("click");
-//大图弹出框
-if(can.pop_up==true){
-	$(can.pic).find('ul li').click(function(){
-		$(can.mhc).height($(document).height()).show();
-		$(can.pop_div).show();
-		popnum = $(this).index();
-		var gdjl_w=-popnum*poppicw;
-		$(can.pop_pic).find('ul').css('left',gdjl_w);
-		popshow(popnum);
-		})
-	$(can.pop_xx).click(function(){
-		$(can.mhc).hide();
-		$(can.pop_div).hide();
-	})
-}
 
 	if(can.autoplay==true){
-//自动播放
+		//自动播放
 		pictime = setInterval(function(){
 			show(tpqhnum);
 			minshow(tpqhnum)
@@ -105,24 +45,10 @@ if(can.pop_up==true){
 			xtqhnum++;
 			if(tpqhnum==picnum){tpqhnum=0};	
 			if(xtqhnum==picminnum){xtqhnum=0};
-					
+						
 		},can.interTime);	
-		
-//鼠标经过停止播放
-		$(can.box).hover(function(){
-			clearInterval(pictime);
-		},function(){
-			pictime = setInterval(function(){
-				show(tpqhnum);
-				minshow(tpqhnum)
-				tpqhnum++;
-				xtqhnum++;
-				if(tpqhnum==picnum){tpqhnum=0};	
-				if(xtqhnum==picminnum){xtqhnum=0};		
-				},can.interTime);			
-			});
 	}
-//小图左右切换			
+	//小图左右切换			
 	$(can.prev_btn).click(function(){
 		if(tpqhnum==0){tpqhnum=picnum};
 		if(xtqhnum==0){xtqhnum=picnum};
@@ -130,7 +56,7 @@ if(can.pop_up==true){
 		tpqhnum--;
 		show(tpqhnum);
 		minshow(xtqhnum);	
-		})
+	})
 	$(can.next_btn).click(function(){
 		if(tpqhnum==picnum-1){tpqhnum=-1};
 		if(xtqhnum==picminnum-1){xtqhnum=-1};
@@ -138,8 +64,8 @@ if(can.pop_up==true){
 		minshow(xtqhnum)
 		tpqhnum++;
 		show(tpqhnum);
-		})	
-//大图左右切换	
+	})	
+	//大图左右切换	
 	$(can.prev).click(function(){
 		if(tpqhnum==0){tpqhnum=picnum};
 		if(xtqhnum==0){xtqhnum=picnum};
@@ -147,7 +73,7 @@ if(can.pop_up==true){
 		tpqhnum--;
 		show(tpqhnum);
 		minshow(xtqhnum);	
-		})
+	})
 	$(can.next).click(function(){
 		if(tpqhnum==picnum-1){tpqhnum=-1};
 		if(xtqhnum==picminnum-1){xtqhnum=-1};
@@ -155,48 +81,26 @@ if(can.pop_up==true){
 		minshow(xtqhnum)
 		tpqhnum++;
 		show(tpqhnum);
-		})
-		
-//小图切换过程
+	})
+	//小图切换过程
 	function minshow(xtqhnum){
+		console.log(xtqhnum);
 		var mingdjl_num =xtqhnum-can.min_picnum+2
 		var mingdjl_w=-mingdjl_num*picminw;
-		var mingdjl_h=-mingdjl_num*picminh;
 		
-		if(can.mindire==true){
-			$(can.pnum).find('ul li').css('float','left');
-			if(picminnum>can.min_picnum){
-				if(xtqhnum<3){mingdjl_w=0;}
-				if(xtqhnum==picminnum-1){
-					mingdjl_w=-(mingdjl_num-1)*picminw;}
-				$(can.pnum).find('ul').stop().animate({'left':mingdjl_w},can.delayTime);
-				}
-				
-		}else{
-			$(can.pnum).find('ul li').css('float','none');
-			if(picminnum>can.min_picnum){
-				if(xtqhnum<3){mingdjl_h=0;}
-				if(xtqhnum==picminnum-1){mingdjl_h=-(mingdjl_num-1)*picminh;}
-				$(can.pnum).find('ul').stop().animate({'top':mingdjl_h},can.delayTime);
-				}
-			}
-
-		
+		if(picminnum>can.min_picnum){
+			if(xtqhnum<3){mingdjl_w=0;}
+			if(xtqhnum==picminnum-1){mingdjl_w=-(mingdjl_num-1)*picminw;}
+			$(can.pnum).find('ul').stop().animate({'marginLeft':mingdjl_w},can.delayTime);
+		}
 	}
-//大图切换过程
-		function show(tpqhnum){
-			var gdjl_w=-tpqhnum*picw;
-			var gdjl_h=-tpqhnum*pich;
-			if(can.picdire==true){
-				$(can.pic).find('ul li').css('float','left');
-				$(can.pic).find('ul').stop().animate({'left':gdjl_w},can.delayTime);
-				}else{
-			$(can.pic).find('ul').stop().animate({'top':gdjl_h},can.delayTime);
-			}//滚动
-			//$(can.pic).find('ul li').eq(tpqhnum).fadeIn(can.delayTime).siblings('li').fadeOut(can.delayTime);//淡入淡出
-			$(can.pnum).find('li').eq(tpqhnum).addClass("on").siblings(this).removeClass("on");
-			$('#text').find('li').addClass('hide').eq(tpqhnum).removeClass('hide');
-		};
-				
+	//大图切换过程
+	function show(tpqhnum){
+		var gdjl_w=-tpqhnum*picw;
+		
+		$(can.pic).find('ul').stop().animate({'marginLeft':gdjl_w},can.delayTime);
+		
+		$(can.pnum).find('li').eq(tpqhnum).addClass("on").siblings(this).removeClass("on");
+	};
 				
 }
